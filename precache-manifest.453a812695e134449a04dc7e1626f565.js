f045f8115d45f4a74f25c5568c216af7adc415a1
Gurleen <gurleenkhalsa03@gmail.com>
2021-09-08 17:14:33 +0530

    Updates

---
+++ precache-manifest.453a812695e134449a04dc7e1626f565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9a241de989e4259274a92340f07516",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53bb7453fd8bd6f9dc3a",
+    "url": "/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "09cc9ce0a99b689b3100",
+    "url": "/static/js/2.944440e5.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.944440e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53bb7453fd8bd6f9dc3a",
+    "url": "/static/js/main.03429f24.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
